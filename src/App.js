e8b3a2bd557756297bacfe5d8aa9100d9e6d3c4e
Sagar Shinde <sagar.shinde1@ihsmarkit.com>
2023-05-04 00:58:19 +0530

    TestCommentFromGamil

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 //import classFunctionEvent from '/Components/classFunctionEvent';
-import FormJs from './Components/Form';
+//import FormJs from './Components/Form';
+import MemoComp from './Components/MemoComp';
 function App() {
   return (
     <div className="App">
-      <FormJs></FormJs>
+      {/*<FormJs></FormJs>*/}
+      <MemoComp name="Jamila"></MemoComp>
     </div>
   );
 }
